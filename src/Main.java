4d8a853cc00a5e68d3a6c0bb0eadfa7dd0bc3d89
yhwit30 <yhwit30@gmail.com>
2023-12-20 11:48:11 +0900

    종료 기능구현

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println(123);
-		
-		
-		
 
+		Scanner sc = new Scanner(System.in);
+
+		new App(sc).run();
+
+		sc.close();
 	}
 }
