2a19ccc29fcdb4c988b14e111538165051fd1de6
yhwit30 <yhwit30@gmail.com>
2023-12-20 13:20:17 +0900

    2단계 1번명언 등록

--- src/App.java
+++ src/App.java
@@ -9,13 +9,19 @@ public class App {
 
 	public void run() {
 		System.out.println("== 명언 앱 실행 ==");
-		// trim() : 혹시 있을지 모르는 좌우 공백 제거
-		while(true) {
+
+		while (true) {
 			System.out.print("명령어 ) ");
 			String cmd = sc.nextLine().trim();
-			
-			if(cmd.equals("종료")) {
+
+			if (cmd.equals("종료")) {
 				break;
+			} else if (cmd.equals("등록")) {
+				System.out.print("명언 : ");
+				String content = sc.nextLine().trim();
+				System.out.print("작가 : ");
+				String author = sc.nextLine().trim();
+				System.out.println("1번 명언이 등록되었습니다.");
 			}
 		}
 
