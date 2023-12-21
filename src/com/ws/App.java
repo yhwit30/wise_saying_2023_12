096a1e0027006094743d12e5e9e0e36e54bdca72
yhwit30 <yhwit30@gmail.com>
2023-12-21 11:42:58 +0900

    8단계 Container를 도입해 구조개선

--- src/com/ws/App.java
+++ src/com/ws/App.java
@@ -1,26 +1,22 @@
 package com.ws;
 
-import java.util.Scanner;
-
 import com.ws.system.controller.SystemController;
 import com.ws.wiseSaying.controller.WiseSayingController;
 
 public class App {
-	private Scanner sc;
 
-	public App(Scanner sc) {
-		this.sc = sc;
+	public App() {
 	}
 
 	public void run() {
 		System.out.println("== 명언 앱 실행 ==");
 
 		SystemController systemController = new SystemController();
-		WiseSayingController wiseSayingController = new WiseSayingController(sc);
+		WiseSayingController wiseSayingController = new WiseSayingController();
 
 		while (true) {
 			System.out.print("명령어 ) ");
-			String cmd = sc.nextLine().trim();
+			String cmd = Container.getScanner().nextLine().trim();
 
 			if (cmd.equals("종료")) {
 				systemController.exit();
@@ -33,6 +29,5 @@ public class App {
 				System.out.println("존재하지 않는 명령어입니다");
 			}
 		}
-
 	}
 }
--- src/com/ws/Main.java
+++ src/com/ws/Main.java
@@ -1,13 +1,12 @@
 package com.ws;
-import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
+		Container.init();
 
-		new App(sc).run();
+		new App().run();
 
-		sc.close();
+		Container.close();
 	}
 }
