3b711b64540145074549a9c61de34356ffaeecbd
yhwit30 <yhwit30@gmail.com>
2023-12-21 10:36:30 +0900

    7단계 리팩토링, SystemController, WiseSayingController로 기능 분리

--- src/com/ws/App.java
+++ src/com/ws/App.java
@@ -1,11 +1,9 @@
 package com.ws;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
-import com.ws.wiseSaying.entity.WiseSaying;
-
+import com.ws.system.controller.SystemController;
+import com.ws.wiseSaying.controller.WiseSayingController;
 
 public class App {
 	private Scanner sc;
@@ -17,40 +15,20 @@ public class App {
 	public void run() {
 		System.out.println("== 명언 앱 실행 ==");
 
-		int lastId = 0;
-		List<WiseSaying> wiseSayings = new ArrayList<>();
+		SystemController systemController = new SystemController();
+		WiseSayingController wiseSayingController = new WiseSayingController(sc);
 
 		while (true) {
 			System.out.print("명령어 ) ");
 			String cmd = sc.nextLine().trim();
 
 			if (cmd.equals("종료")) {
+				systemController.exit();
 				break;
 			} else if (cmd.equals("등록")) {
-				int id = lastId + 1;
-				System.out.print("명언 : ");
-				String content = sc.nextLine().trim();
-				System.out.print("작가 : ");
-				String author = sc.nextLine().trim();
-
-				WiseSaying wiseSaying = new WiseSaying(id, content, author);
-				wiseSayings.add(wiseSaying);
-
-				System.out.printf("%d번 명언이 등록되었습니다.\n", id);
-				lastId++;// 마지막 번호 증가
-
+				wiseSayingController.write();
 			} else if (cmd.equals("목록")) {
-				if (wiseSayings.size() == 0) {
-					System.out.println("등록 된 명언이 없어");
-				} else {
-					for (int i = wiseSayings.size() - 1; i >= 0; i--) {
-						WiseSaying a = wiseSayings.get(i);
-						System.out.println(a.getId() + "/" + a.getAuthor() + "/" + a.getContent());
-
-					}
-
-					System.out.println("등록 된 명언 수 : " + wiseSayings.size());
-				}
+				wiseSayingController.list();
 			} else {
 				System.out.println("존재하지 않는 명령어입니다");
 			}
--- src/com/ws/system/controller/SystemController.java
+++ src/com/ws/system/controller/SystemController.java
@@ -2,4 +2,9 @@ package com.ws.system.controller;
 
 public class SystemController {
 
+	public void exit() {
+		System.out.println("== 명언 앱 종료 ==");
+		
+	}
+
 }
