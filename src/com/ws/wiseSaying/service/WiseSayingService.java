9b5891d4cdfbe2a8c410b6769d7935681d31ee75
yhwit30 <yhwit30@gmail.com>
2023-12-22 15:55:39 +0900

    18단계 리팩토링, WiseSayingServise의 리포지터리 로직을 WiseSayingRepository로 이전

--- src/com/ws/wiseSaying/service/WiseSayingService.java
+++ src/com/ws/wiseSaying/service/WiseSayingService.java
@@ -1,53 +1,38 @@
 package com.ws.wiseSaying.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.ws.wiseSaying.entity.WiseSaying;
+import com.ws.wiseSaying.repository.WiseSayingRepository;
 
 public class WiseSayingService {
-	private int lastWiseSayingId;
-	private List<WiseSaying> wiseSayings;
+
+	private WiseSayingRepository wiseSayingRepository;
 
 	public WiseSayingService() {
 
-		lastWiseSayingId = 0;
-		wiseSayings = new ArrayList<>();
+		wiseSayingRepository = new WiseSayingRepository();
 	}
 
 	public List<WiseSaying> findAll() {
-		return wiseSayings;
-	}
-
-	public WiseSaying findById(int id) {
-		for (WiseSaying wiseSaying : wiseSayings) {
-			if (wiseSaying.getId() == id) {
-				return wiseSaying;
-			}
-		}
-
-		return null;
+		return wiseSayingRepository.finAll();
 	}
 
 	public int write(String content, String author) {
-		int id = lastWiseSayingId + 1;
-
-		WiseSaying wiseSaying = new WiseSaying(id, content, author);
-		wiseSayings.add(wiseSaying);
-
-		lastWiseSayingId = id; // 방금 전에 새 명언이 생겼으니, lastWiseSayingId의 값을 갱신
-
-		return id;
+		return wiseSayingRepository.write(content, author);
 	}
 
 	public void remove(WiseSaying wiseSaying) {
-		wiseSayings.remove(wiseSaying);
+		wiseSayingRepository.remove(wiseSaying);
 	}
 
 	public void modify(WiseSaying wiseSaying, String content, String author) {
-		wiseSaying.setContent(content);
-		wiseSaying.setAuthor(author);
+		wiseSayingRepository.modfy(wiseSaying, content, author);
 
+	}
+
+	public WiseSaying findById(int id) {
+		return wiseSayingRepository.findById(id);
 	}
 
 }
